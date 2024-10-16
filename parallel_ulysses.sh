41d101ed94ea6fbb7e17c1fb46d5ad618f021ec5
simone <divo.simone@gmail.com>
2024-10-16 12:45:40 +0200

    updated bash colors

--- parallel_ulysses.sh
+++ parallel_ulysses.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-ORANGE='\033[0;33m'
-NC='\033[0m' # No Color
-GREEN='\033[0;32m'
+ORANGE=$ì'\033[0;33m'
+NC=$'\033[0m' # No Color
+GREEN=$'\033[0;32m'
 
 echo "${ORANGE} started processing $*.. ${NC}"
 python3 gpgomea_experiments_no_list.py $*;
