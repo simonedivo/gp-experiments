8ccb00c9c178a45cd5676011b9e409e8baaf3ca2
simone <divo.simone@gmail.com>
2024-10-16 12:58:57 +0200

    updated colors

--- parallel.sh
+++ parallel.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-echo "started processing $*.."
+YELLOW=$'\033[0;33m'
+NC=$'\033[0m' # No Color
+GREEN=$'\033[0;32m'
+
+echo "${YELLOW} started processing $*.. ${NC}"
 python3 gpgomea_experiments_no_list.py $*;
-echo finished processing "$*";
+echo "${GREEN}finished processing $* ${NC}";
+
