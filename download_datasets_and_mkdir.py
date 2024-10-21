712f38b134ce856573b2740a3e1126266f4f18a1
simone <divo.simone@gmail.com>
2024-10-21 14:45:07 +0200

    chmod added on download_datasets_and_mkdir.py

--- download_datasets_and_mkdir.py
+++ download_datasets_and_mkdir.py
@@ -8,14 +8,17 @@ print("Creating folders...")
 result_path = 'GPGomea/results'
 if not os.path.exists(result_path):
     os.makedirs(result_path)
+os.chmod(result_path, 0o777)
 
 progress_log_path = 'GPGomea/progress_logs'
 if not os.path.exists(progress_log_path):
     os.makedirs(progress_log_path)
+os.chmod(result_path, 0o777)
 
 finished_runs_path = 'GPGomea/finished_runs'
 if not os.path.exists(finished_runs_path):
     os.makedirs(finished_runs_path)
+os.chmod(result_path, 0o777)
 print("Folders created!")
 
 
