b3947f5a627977bbc874df3324057807879b0cf8
simone <divo.simone@gmail.com>
2024-11-05 16:01:23 +0100

    Added new ulysses experiments

---
+++ big_execute_parallel_ulysses/big_execute_parallel_ulysses_444942.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+parallel --jobs ${1} --colsep ',' --ungroup ./parallel_ulysses.sh {1} {2} {3} {4} {5} {6} :::: big_input_args_ulysses/big_input_args_ulysses_444942.txt
--- execute_parallel.sh
+++ execute_parallel.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cp ./new_input_args.txt ./new_input_args_updated.txt
+#cp ./new_input_args.txt ./new_input_args_updated.txt
 
 parallel --jobs ${1} --colsep ',' --ungroup ./parallel.sh {1} {2} {3} {4} {5} {6} :::: new_input_args.txt
