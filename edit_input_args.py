bfcca5ac2ce26ca93e349e62d8da02fb423e213f
simone <divo.simone@gmail.com>
2024-11-07 12:19:17 +0100

    new input cm10

--- edit_input_args.py
+++ edit_input_args.py
@@ -8,12 +8,12 @@ generations = int(sys.argv[5])
 
 string_to_remove = f"{seed},{dataset_name},{training_set_dimension},{popsize},{generations}\n"
 
-with open("new_input_args_updated.txt", "r") as f:
+with open("btbpg_input_args_updated.txt", "r") as f:
     full_txt = f.read()
     f.close()
 
 modified_txt = full_txt.replace(string_to_remove, "")
 
-with open("new_input_args_updated.txt", "w") as f:
+with open("btbpg_input_args_updated.txt", "w") as f:
     f.write(modified_txt)
     f.close()
--- parallel.sh
+++ parallel.sh
@@ -6,5 +6,5 @@ GREEN=$'\033[0;32m'
 
 echo "${YELLOW} started processing $*.. ${NC}"
 python3 gpgomea_experiments_no_list.py $*;
-#python3 edit_input_args.py $*;
+python3 edit_input_args.py $*;
 echo "${GREEN}finished processing $* ${NC}";
