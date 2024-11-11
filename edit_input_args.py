10b70b1f60af6c95752d4337125f52a6300a4548
simone <divo.simone@gmail.com>
2024-11-11 15:06:48 +0100

    added generalization functionalities to the code

--- edit_input_args.py
+++ edit_input_args.py
@@ -5,15 +5,16 @@ dataset_name = str(sys.argv[2])
 training_set_dimension = int(sys.argv[3])
 popsize = int(sys.argv[4])
 generations = int(sys.argv[5])
+txt_to_update = str(sys.argv[6])
 
 string_to_remove = f"{seed},{dataset_name},{training_set_dimension},{popsize},{generations}\n"
 
-with open("btbpg_input_args_updated.txt", "r") as f:
+with open(txt_to_update, "r") as f:
     full_txt = f.read()
     f.close()
 
 modified_txt = full_txt.replace(string_to_remove, "")
 
-with open("btbpg_input_args_updated.txt", "w") as f:
+with open(txt_to_update, "w") as f:
     f.write(modified_txt)
     f.close()
