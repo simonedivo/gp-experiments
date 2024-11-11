d896049fbf50f2ceb94729f8baa8e66c755bf41e
simone <divo.simone@gmail.com>
2024-11-11 15:25:41 +0100

    improved generalization

--- edit_input_args.py
+++ edit_input_args.py
@@ -9,15 +9,15 @@ generations = int(sys.argv[5])
 txt_to_update = str(sys.argv[6])
 
 lock = threading.Lock()
-
 string_to_remove = f"{seed},{dataset_name},{training_set_dimension},{popsize},{generations}\n"
+
 with lock:
     with open(txt_to_update, "r") as f:
         full_txt = f.read()
         f.close()
-    
+
     modified_txt = full_txt.replace(string_to_remove, "")
-    
+
     with open(txt_to_update, "w") as f:
         f.write(modified_txt)
         f.close()
