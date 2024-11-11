af97161dc46575ba310a31f8550a74d671fc511c
simone <divo.simone@gmail.com>
2024-11-11 15:17:08 +0100

    Improved generalization

--- edit_input_args.py
+++ edit_input_args.py
@@ -1,4 +1,5 @@
 import sys
+import threading
 
 seed = int(sys.argv[1])
 dataset_name = str(sys.argv[2])
@@ -7,14 +8,16 @@ popsize = int(sys.argv[4])
 generations = int(sys.argv[5])
 txt_to_update = str(sys.argv[6])
 
+lock = threading.Lock()
+
 string_to_remove = f"{seed},{dataset_name},{training_set_dimension},{popsize},{generations}\n"
-
-with open(txt_to_update, "r") as f:
-    full_txt = f.read()
-    f.close()
-
-modified_txt = full_txt.replace(string_to_remove, "")
-
-with open(txt_to_update, "w") as f:
-    f.write(modified_txt)
-    f.close()
+with lock:
+    with open(txt_to_update, "r") as f:
+        full_txt = f.read()
+        f.close()
+    
+    modified_txt = full_txt.replace(string_to_remove, "")
+    
+    with open(txt_to_update, "w") as f:
+        f.write(modified_txt)
+        f.close()
--- execute_parallel_google.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-#cp input_args_google/input_args_google.txt input_args_google/input_args_google_updated.txt
-
-parallel --jobs ${1} --colsep ',' --ungroup ./parallel_google.sh {1} {2} {3} {4} {5} {6} :::: input_args_google/input_args_google.txt
